140d59c311b7a7f75e2ea16b880b1f40c960da24
Hannes Bröhl <hannes.broehl@icloud.com>
2023-10-07 11:07:59 +0200

    todo-App-with-State

---
+++ with State-Management/dateScript.js
@@ -0,0 +1,5 @@
+const dateMEZ = new Date();
+console.log(dateMEZ); 
+
+document.getElementById('time-heading').innerHTML = dateMEZ.toLocaleDateString('de-DE');
+
---
+++ with State-Management/todoScript.js
@@ -0,0 +1,74 @@
+ // Hello Hannes
+
+const todoDescription = document.querySelector('#todo-description');
+const btnAdd = document.querySelector('#btn-add');
+const btnClear = document.querySelector('#btn-clear');
+
+
+//check if something is in the state 
+const state = JSON.parse(localStorage.getItem('state')) || {
+    todos: [],
+};
+
+
+btnAdd.addEventListener('click', addTodo);
+
+
+function addTodo(e) {
+    e.preventDefault(); //button automatically reloads the page & preventDefault stops this 
+
+    const newTodo = {
+        description: todoDescription.value,
+        done: false,
+    }
+
+    state.todos.push(newTodo);
+    localStorage.setItem('state', JSON.stringify(state))
+    todoDescription.value = '';
+    todoDescription.focus(); //inputfield stays active
+}
+
+//show todos on app
+function renderTodos() {
+    const todosList = document.querySelector('#todos-list');
+    
+    for (const todo of state.todos) {
+
+        //create <li> 
+        const newTodoLi = document.createElement('li');
+
+        //create <input> 
+       const doneCheckbox = document.createElement('input');
+
+       //<input type='checkbox'>
+       doneCheckbox.type = 'checkbox';
+
+       //<input type='checkbox' id=''
+       doneCheckbox.id = todo.description.toLowerCase().replaceAll(' ', '') + Math.floor(Math.random() * 145681465); //each new todo gets new id
+
+       //<input type='checkbox' id=''>
+       doneCheckbox.checked = todo.done; //checked if true
+
+       //create <label> 
+       const todoLabel = document.createElement('label');
+
+       // <label for=''>
+       todoLabel.setAttribute('for', doneCheckbox.id);
+
+       //<label> Description </label>
+       todoLabel.innerText = todo.description;
+
+       //<input type='checkbox'> & append <label> on <li> 
+       newTodoLi.append(doneCheckbox, todoLabel);
+
+       //<li> append on <ul> (as child-Element)
+       todosList.append(newTodoLi);
+    }
+};
+renderTodos();
+
+//clear LocalStorage
+btnClear.addEventListener('click', () => {
+    localStorage.clear();
+    console.log('clear LS');
+});
